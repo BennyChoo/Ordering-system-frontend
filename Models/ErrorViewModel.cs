38e881c007337248ceb48262aa7cb678895aa9bc
BennyChoo <wai.pan@kvdholdings.com.my>
2023-12-06 21:55:57 +0800

    added landing page and menu detail page

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Front_end___ordering.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}

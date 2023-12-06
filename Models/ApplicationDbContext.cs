6e61fed98dde62deff09b1ab9e324f41461aa3ae
BennyChoo <143769079+BennyChoo@users.noreply.github.com>
2023-12-06 12:20:18 +0800

    Add files via upload
    first commit

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace QFD.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+        //public virtual DbSet<Menus> Menus { get; set; }
+
+       
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+
+        }
+
+
+
+    }
+}

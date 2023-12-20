cbe4f8fcbdcb7055169afbeb1df3ff605b2389eb
BennyChoo <wai.pan@kvdholdings.com.my>
2023-12-20 10:17:06 +0800

    when changing product quantity in checkout page, the product price wont change only the subtotal price change

---
+++ Front end - ordering/ViewComponents/VariationsViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Front_end___ordering.ViewComponents
+{
+    public class VariationsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string variationsType)
+        {
+            if (variationsType == "AddOns")
+            {
+                return View("AddOns");
+            }
+            else if (variationsType == "ChangePart")
+            {
+                return View("ChangePart");
+            }
+            else if (variationsType == "Takeaway")
+            {
+                return View("Takeaway");
+            }
+            
+
+            return View();
+        }
+    }
+}
--- Front end - ordering/obj/Debug/net7.0/Front end - ordering.AssemblyInfo.cs
+++ Front end - ordering/obj/Debug/net7.0/Front end - ordering.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Front end - ordering")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6850e018bc54bd48f3ce33fe7d9b0bbb39a02d78")]
 [assembly: System.Reflection.AssemblyProductAttribute("Front end - ordering")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Front end - ordering")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
